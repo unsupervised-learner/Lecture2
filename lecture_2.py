86de405d50c1c7270cb2aacf8ab16f9c51d4514e
unsupervised-learner <vernonvernon29@gmail.com>
2019-08-20 15:51:55 +0200

    pulling yahoo stock data

--- lecture_2.py
+++ lecture_2.py
@@ -1,5 +1,16 @@
+import pandas as pd
+import pandas_datareader.data as pdr
+import datetime as dt
+
 def main():
-	print('Hello Wordl!')
+	start_date = dt.datetime(2010, 1, 1)
+	end_date = dt.datetime(2019, 6, 24)
+	print('start date : {}'.format(start_date))
+	print('end date : {}'.format(end_date))
+
+	#download data
+	stocks = pdr.DataReader('MMM', 'yahoo', start_date, end_date)
+	print(stocks)
 
 
 if __name__ == '__main__':
